b3168f43303d95766a992a12923cb645a11f9985
tianyi ma <130447490+addwing@users.noreply.github.com>
2023-11-03 02:43:38 +0800

    Update layout.tsx
    修改网站标题、信息

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "AI智能量化投研助手",
+  description: "投研助手",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "AI智能量化投研助手",
     statusBarStyle: "default",
   },
 };
