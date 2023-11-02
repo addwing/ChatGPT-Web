7ef025fcdae00ceb0eae344004dc87b7a6c024d2
tianyi ma <130447490+addwing@users.noreply.github.com>
2023-11-03 04:05:32 +0800

    Update cn.ts
    Modify preset words

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -310,7 +310,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "我是您的研投助手，可以为您提供国内最新的股票和基金信息，有什么可以帮你的吗",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
